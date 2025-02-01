213450b50b89f3dc29a8752fe10c3ed804b2e0a3
Joksiel <siel3@yahoo.com>
2025-02-01 22:50:00 +0200

    colors 2 build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ sudo apt update && sudo apt install nasm binutils -y
 echo -e "\e[32mbuilding animation...\e[0m"
 nasm -f elf64 asmination.asm -o asmination.o
 ld asmination.o -o asmination
-echo -e "\e[32m play animation with ./asmination !\e[0m"
+echo -e "\e[35m play animation with ./asmination\e[0m"
