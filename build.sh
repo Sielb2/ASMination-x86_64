c9368958fbc7a0d3e59668b032c096f28fc3f648
Joksiel <siel3@yahoo.com>
2025-02-01 22:41:29 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "installing..."
+sudo apt update && sudo apt install nasm binutils -y
+echo "building animation..."
 nasm -f elf64 asmination.asm -o asmination.o
 ld asmination.o -o asmination
-echo "run animation with ./asmination"
+echo "run animation with ./asmination !"
